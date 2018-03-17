10904412de5c65f822d56258606322e49e0f1fce
AzmiFaiz <amfibifaiz@gmail.com>
2018-03-17 19:06:39 +0700

    tambah htaccess

--- index.php
+++ index.php
@@ -69,7 +69,7 @@ if(is_array($data['events'])){
             {
                 // send same message as reply to user
                 $result = $bot->replyText($event['replyToken'], $event['message']['text']);
- 
+                $result = $bot->replyText($event['replyToken'], "Hai bro");
                 // or we can use replyMessage() instead to send reply message
                 // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
                 // $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
