405a6e48ac4f12167dba4b1b89b648a03f6b085f
AzmiFaiz <amfibifaiz@gmail.com>
2018-03-17 19:08:58 +0700

    tambah kode aplikasi

--- index.php
+++ index.php
@@ -68,7 +68,7 @@ if(is_array($data['events'])){
             if($event['message']['type'] == 'text')
             {
                 // send same message as reply to user
-                $result = $bot->replyText($event['replyToken'], $event['message']['text']);
+                // $result = $bot->replyText($event['replyToken'], $event['message']['text']);
                 $result = $bot->replyText($event['replyToken'], "Hai bro");
                 // or we can use replyMessage() instead to send reply message
                 // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
